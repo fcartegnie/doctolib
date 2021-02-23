8ff7e0f6fdc0fa55efc4a43283d195426ee3421e
Francois Cartegnie <fcvlcdev@free.fr>
2021-02-23 14:46:56 +0100

    use json only, remove mail, use stdout for automation

--- README.md
+++ README.md
@@ -1,48 +1,21 @@
-# Requirements
-beautifulsoup (`aptitude install python3-bs4`)
+# doctolib monitoring
 
-make sure that the fr_FR.utf8 locales are installed
+This script checks approx every 1.5 hours whether an appointement for a doctor is free before a set date.
 
-# doctolib
-
-This script checks every x seconds (between 10 and 60 seconds) whether an appointement for a doctor is free before a set date.
-
-If a date has been freed, it will send a mail to tell the user.
-
-Currently works with the university of lille 3 mails but can be easily changed to work with any smtps compatible mail server.
+If a date is available, it outputs on stdout and exits otherwise outputs on stderr.
 
 Usage :
 ```
-usage: doctolib.py [-h] -t TO -f SENDER -d DATE -u URL
+usage: doctolib.py [-h] -d DATE -u URL
 
 optional arguments:
   -h, --help            show this help message and exit
-  -t TO, --to TO        set the mail of the receiver
-  -f SENDER, --from SENDER
-                        set the mail of the sender
-  -d DATE, --date DATE  format : dd-mm-yyyy. Sets the date before which we
-                        send a mail
+  -d DATE, --date DATE  format : dd-mm-yyyy.
   -u URL, --url URL     set the url to check
 ```
 
-# To run in the background
-To suspend : `Ctrl Z`
-
-To start in background : `bg %[job]`
-
-To remove from the terminal : `disown %[job]`
-
-# How to get the URL
+# URL
 Go on the URL of the doctor you want an appointement with
 
-Right click on the page and go to Network
-
-Reload
-
-Change the week for the appointement
-
-On the network window, click on the last GET and look at the address on the right panel ("Request URL").
-
-Use this URL as the -u argument (you can change the date if needed).
-
+Use the availabilities.json URL from the doctor's booking page
 
--- doctolib.py
+++ doctolib.py
@@ -1,129 +1,44 @@
-# needs beautifulsoup (aptitude install python3-bs4)
-# make sure that the fr_FR.utf8 locales are installed
-
-import urllib.request
-from bs4 import BeautifulSoup
-import smtplib
+from urllib.request import urlopen, Request
 from datetime import datetime
-import locale
 import argparse
 import sys
 from random import randint
 import time
-from getpass import getpass
 import json
-import os
-
-# we make sure that we use the french locale
-locale.setlocale(locale.LC_ALL, 'fr_FR.utf8')
 
 receivers = []
 
-message = """From: From You You You <from@fromdomain.com>
-To: To Person <to@todomain.com>
-Subject: doclib is free. Hurry up !
-
-"""
-
-log_file = "log.txt"
-
-def my_print(text):
-    if os.path.exists(log_file):
-        with open(log_file, "a") as f:
-            f.write(text + "\n")
-
-    else:
-        with open("log.txt", "w") as f:
-            f.write(text + "\n")
-
-def send_mail(sender, receivers, message, psswd):
-    '''
-    Sends a mail
-    '''
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(sender, psswd)
-    server.sendmail(sender, receivers, message)
-    server.quit()
-    my_print("Successfully sent email")
-
-
-def get_page(url):
-    '''
-    Get the html of the url
-    '''
-    page_html = urllib.request.urlopen(url)
-    return page_html
-
-
-def get_next_appointement(content):
-    '''
-    Get the next date available
-    '''
-    soup = BeautifulSoup(content, "lxml")
-    dates = soup.findAll("p")
-    jsonDate = json.loads(str(dates[0])[3:-4])
-    return datetime.strptime(jsonDate["next_slot"], '%Y-%m-%d')
-
-#send_mail(sender, receivers, message)
-
+def get_next_appointement(url):
+    jsonread = urlopen(Request(url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36', 'Accept': 'application/json'})).read()
+    jsonDate = json.loads(jsonread)
+    return datetime.strptime(jsonDate['next_slot'], '%Y-%m-%d')
 
 def main():
     # Install the argument parser. Initiate the description with the docstring
     argparser = argparse.ArgumentParser(
         description=sys.modules[__name__].__doc__)
     # this is a option which need one extra argument
-    argparser.add_argument("-t",
-                           '--to',
-                           required=True,
-                           help="set the mail of the receiver")
-    argparser.add_argument("-f",
-                           '--from',
-                           required=True,
-                           dest="sender",
-                           help="set the mail of the sender")
-    argparser.add_argument('-d',
-                           "--date",
-                           required=True,
-                           help="""format : dd-mm-yyyy. Sets the date before
-                            which we send a mail""")
     argparser.add_argument('-u',
                            "--url",
                            required=True,
                            help="set the url to check")
+    argparser.add_argument('-d',
+                           "--date",
+                           required=True,
+                           help="format : dd-mm-yyyy")
 
     arguments = argparser.parse_args()
-
-    # sets the arguments
-    receivers.append(arguments.to)
-    sender = arguments.sender
-    url = arguments.url
-    content = get_page(url)
-    next_appointement = get_next_appointement(content)
     appointement_wanted = datetime.strptime(arguments.date, '%d-%m-%Y')
 
-    auth_pwd = getpass('SMTP password for the sender')
-
     while True:
+        next_appointement = get_next_appointement(arguments.url)
         if(appointement_wanted > next_appointement):
-            send_mail(sender, receivers, message, auth_pwd)
-            with open(log_file, "a") as f:
-                f.write("appointement wanted = {}\n".format(appointement_wanted))
-                f.write("next appointement = {}\n".format(next_appointement))
-      
+            sys.stdout.write("appointement wanted = {}\n".format(appointement_wanted))
+            sys.stdout.write("next appointement = {}\n".format(next_appointement))
             sys.exit()
         else:
-            content = get_page(url)
-            next_appointement = get_next_appointement(content)
-            with open("log.txt", "w") as f:
-                f.write("{}: {}\n".format(time.strftime("%H:%M:%S"), next_appointement))
-            time.sleep(randint(10, 60))
+            sys.stderr.write("{}: {}\n".format(time.strftime("%H:%M:%S"), next_appointement))
+            time.sleep(3600 + randint(0, 1500))
 
-# This is a Python's special:
-# The only way to tell wether we are running the program as a binary,
-# or if it was imported as a module is to check the content of
-# __name__.
-# If it is `__main__`, then we are running the program
-# standalone, and we run the main() function.
 if __name__ == "__main__":
     main()
